a0b67416bd6f8bb5e0670731c7727cb724b1bb00
ravibdf <ravi.bdf@gmail.com>
2025-04-09 20:34:08 -0400

    simulating a blockchain transfer

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,27 @@ impl Runtime {
     }
 }
 
-fn main() {
-    
+fn main() {    
+    let mut runtime = Runtime::new();
+    let alice = "alice".to_string();
+    let bob = "bob".to_string();
+    let charlie = "charlie".to_string();
+
+    runtime.balances.set_balance(alice.clone(), 100);
+
+    runtime.system.increment_block_number();
+    assert!(runtime.system.block_number() == 1);
+
+    runtime.system.increment_nonce(&alice);
+    if let Err(e) = runtime.balances.transfer(alice.clone(), bob.clone(), 30) {
+        eprintln!("Transfer failed: {}", e);
+    }
+
+    runtime.system.increment_nonce(&alice);
+    if let Err(e) = runtime.balances.transfer(alice.clone(), charlie.clone(), 20) {
+        eprintln!("Transfer failed: {}", e);
+    }
+
+    println!("Alice's balances: {}", runtime.balances.balance(alice.clone()));
+
 }
